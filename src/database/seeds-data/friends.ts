dc51b5fe50250c62a045fee3c9b323bcbc55090c
abdulgilani <ah-gilani@hotmail.com>
2025-01-12 22:18:51 -0500

    Edit the seeding

---
+++ src/database/seeds-data/friends.ts
@@ -0,0 +1,35 @@
+export default [ 
+    {
+        "id": "550e8400-e29b-41d4-a716-446655440002",
+        "name": "Samantha Lee",
+        "email": "samantha.lee@example.com",
+        "password": "$2b$12$Dh5iFGIu6nX/bMTa26E.dudK8XtSuRcxExnqYZwnJ7pG.EVO3D8dC",
+        "neurodivergent_disorders": ["Autism Spectrum Disorder"],
+        "description": "Software engineer with a focus on backend systems. I enjoy coding and find peace in solving logical puzzles.",
+        "occupation": "Software Engineer",
+        "location": { "latitude": 37.774929, "longitude": -122.419418 },
+        "photo": "https://randomuser.me/api/portraits/women/2.jpg"
+      },
+      {
+        "id": "550e8400-e29b-41d4-a716-446655440005",
+        "name": "Michael Brown",
+        "email": "michael.brown@example.com",
+        "password": "$2b$12$6uOft6CZ8XVYKQuYNo/KtudW4Vo9PVqlE3oRUwTeW0lRV9BNFZ/q2",
+        "neurodivergent_disorders": ["Autism Spectrum Disorder", "ADHD"],
+        "description": "Tech enthusiast and hobbyist programmer with a knack for learning new technologies.",
+        "occupation": "IT Consultant",
+        "location": { "latitude": 51.507351, "longitude": -0.127758 },
+        "photo": "https://randomuser.me/api/portraits/men/7.jpg"
+      },
+      {
+        "id": "550e8400-e29b-41d4-a716-446655440006",
+        "name": "Olivia Martinez",
+        "email": "olivia.martinez@example.com",
+        "password": "$2b$12$PfYkRq/F6hvsfdB3PGzDhOnmAd4KpsmZeNcM8AK0fBL3gF7dhhKFC",
+        "neurodivergent_disorders": ["Autism Spectrum Disorder"],
+        "description": "A passionate artist who expresses emotions through abstract paintings and sculptures.",
+        "occupation": "Artist",
+        "location": { "latitude": 41.878113, "longitude": -87.629799 },
+        "photo": "https://randomuser.me/api/portraits/women/8.jpg"
+      }
+]
